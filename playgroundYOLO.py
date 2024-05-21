d0e2b9814139d158b88e1d84dc6ca3a9b42d4163
oyaawn <ayan.chatterjee@mailbox.tu-dresden.de>
2024-05-21 04:01:15 +0200

    1st commit

---
+++ playgroundYOLO.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+import torch
+if torch.backends.mps.is_available():
+    mps_device = torch.device("mps")
+    x = torch.ones(1, device=mps_device)
+    print (x)
+else:
+    print ("MPS device not found.")
+
+
+model = YOLO('yolov8l')
+# model.predict('clips/0a2d9b_0.mp4',save=True)
+results= model.predict('clips/0a2d9b_0.mp4', save=True, device=mps_device)
+print(results[0])
+print('--------------------------------')
+for box in results[0].boxes:
+    print(box)
+
